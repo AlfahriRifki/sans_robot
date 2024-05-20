7e09bfe66cfdb07c5b6705f5b0172958d3ecc835
AlfahriRifki <alfahri.rifki@gmail.com>
2024-05-20 16:06:56 +0700

    progress joystick drive

--- launch/joystick.launch.py
+++ launch/joystick.launch.py
@@ -11,19 +11,19 @@ def generate_launch_description():
 
     joy_params = os.path.join(get_package_share_directory('articubot_one'),'config','joystick.yaml')
 
-    # joy_node = Node(
-    #         package='joy',
-    #         executable='joy_node',
-    #         parameters=[joy_params],
-    #         # parameters=[joy_params, {'use_sim_time': use_sim_time}],
-    #      )
+    joy_node = Node(
+            package='joy',
+            executable='joy_node',
+            parameters=[joy_params],
+            # parameters=[joy_params, {'use_sim_time': use_sim_time}],
+         )
 
     teleop_node = Node(
             package='teleop_twist_joy',
             executable='teleop_node',
             name='teleop_node',
             parameters=[joy_params],
-            # remappings=[('/cmd_vel','/diff_cont/cmd_vel_unstamped')]
+            remappings=[('/cmd_vel','/diff_cont/cmd_vel_unstamped')]
             # remappings=[('/cmd_vel','/cmd_vel_joy')]
          )
 
@@ -41,7 +41,7 @@ def generate_launch_description():
             'use_sim_time',
             default_value='false',
             description='Use sim time if true'),
-        # joy_node,
+        joy_node,
         teleop_node,
         # twist_stamper       
     ])
